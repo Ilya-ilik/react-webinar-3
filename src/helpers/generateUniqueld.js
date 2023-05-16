7bf2e48abc33c2a0c9f2ff7ab7a7c97d83ad5fb7
Ilya-ilik <101934601+Ilya-ilik@users.noreply.github.com>
2023-05-16 15:35:21 +0300

    Second commit

--- src/helpers/generateUniqueld.js
+++ src/helpers/generateUniqueld.js
@@ -6,6 +6,7 @@ const GenerateUniqueId = (numbersArray) => {
     const newId = Math.round(Math.random()*100);
     // Сделаем проверку на уникальность пускай она принимает в себя массив из всех айдишников записей
     if (numbersArray.includes(newId)) { // И если число которое она сгенерировала в нем есть запускает себя заново
+        return 0,
         GenerateUniqueId(numbersArray);
     } else {
         return newId; // Рано или поздно она дойдет до числа которого в списке нет - и вернёт его
